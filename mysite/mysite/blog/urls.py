a0993b9f6a60a981ec0ef2fb782bb55ce44e4d35
pjpjpj <877302410@qq.com>
2018-07-19 22:17:20 +0800

    update mysite css

--- mysite/mysite/blog/urls.py
+++ mysite/mysite/blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
 	# http://localhost:8000/blog/1
+	path('', views.blog_list, name='blog_list'),
 	path('<int:blog_pk>', views.blog_detail, name="blog_detail"),
-	path('type/<int:blogs_type_pk>', views.blogs_with_type, name="blogs_with_type")
+	path('type/<int:blogs_type_pk>', views.blogs_with_type, name="blogs_with_type"),
 ]	
