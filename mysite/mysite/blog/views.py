99996442491a675dd7b7b81dc47971202a938d68
pjpjpj <877302410@qq.com>
2018-07-20 17:03:54 +0800

    update mysite of bootstrap

--- mysite/mysite/blog/views.py
+++ mysite/mysite/blog/views.py
@@ -4,7 +4,7 @@ from .models import Blog, BlogType
 def blog_list(request):
 	context = {}
 	context['blogs'] = Blog.objects.all()
-	context['blogs_count'] = Blog.objects.all().count()
+	context['blog_types'] = BlogType.objects.all()
 	return render_to_response('blog/blog_list.html', context)
 
 def blog_detail(request, blog_pk):
@@ -17,5 +17,6 @@ def blogs_with_type(request, blogs_type_pk):
 	blog_type = get_object_or_404(BlogType, pk=blogs_type_pk)
 	context['blogs'] = Blog.objects.filter(blog_type=blog_type)
 	context['blog_type'] = blog_type
+	context['blog_types'] = BlogType.objects.all()
 	return render_to_response('blog/blogs_with_type.html', context)
 
