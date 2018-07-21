0509b82d81686341336221657b83bcc3ae636aa9
pjpjpj <877302410@qq.com>
2018-07-21 21:17:09 +0800

    update mysite of richTextEditor

--- mysite/mysite/blog/urls.py
+++ mysite/mysite/blog/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	# http://localhost:8000/blog/1
-	path('', views.blog_list, name='blog_list'),
-	path('<int:blog_pk>', views.blog_detail, name="blog_detail"),
-	path('type/<int:blogs_type_pk>', views.blogs_with_type, name="blogs_with_type"),
-]	
+    # http://localhost:8000/blog/1
+    path('', views.blog_list, name='blog_list'),
+    path('<int:blog_pk>', views.blog_detail, name="blog_detail"),
+    path('type/<int:blogs_type_pk>', views.blogs_with_type, name="blogs_with_type"),
+	path('date/<int:year>/<int:month>', views.blogs_with_date, name="blog_with_date"),
+]
